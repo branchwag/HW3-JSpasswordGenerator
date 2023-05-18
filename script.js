39d57341e2598373489b432c12c5b78d767eb0f3
LeAnne B <branchleannelmb@gmail.com>
2023-05-18 01:27:56 -0400

    condenses code to fewer functions

--- script.js
+++ script.js
@@ -1,90 +1,75 @@
 // Assignment code here
 
 function generatePassword() {
-  // Make this an object maybe? Nah for now
-  //var passparams = {
+
   var passwordLength = prompt("Please input a password length (at least 8 characters but no more than 128 characters).");
   var includeLowercase = confirm("Include lowercase letters?");
   var includeUppercase = confirm("Include uppercase letters?");
   var includeNumeric = confirm("Include numbers?");
   var includeSpecial = confirm("Include special characters?");
-  // };
+
   // var generatedPass = "JustTesting";
 
   //just logging to confirm if result of confirm is bool....
-  //console.log(includeLowercase);
+  // console.log(includeLowercase);
 
-    //check inputs
+    //Check inputs
   // the password needs to be a valid length 
-  // if (passwordLength < 8 || passwordLength > 128) {
-  //   passwordLength = prompt("Password length invalid. Please enter number between 8 and 128.");
-  // }
+  if (passwordLength < 8 || passwordLength > 128) {
+    passwordLength = prompt("Password length invalid. Please enter number between 8 and 128.");
+  }
 
   // make function to make the actual password and call it only if tests check out
   var generatedPass = [];
+  var eligiblevalues = [];
+
+  //making the arrays we will need, starting from strings...
   var specialCharsString = "!@#$%^&*(){}[]=<>/,.";
   //var specialChars = " !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
   //how to use ALL special chars in string since everything you would need to escape them is in the string? Evil! ;/
   var lowercaseCharsString = "abcdefghijklmnopqrstuvwxyz";
   var numbersString = "0123456789";
-  var uppercaseCharsString = lowercaseCharsString.toUpperCase()
+  var uppercaseCharsString = lowercaseCharsString.toUpperCase();
 
-  //convert all of the strings of stuff into arrays
+  //converting all of the strings of stuff into arrays
   var specialCharArray = specialCharsString.split('');
   var lowercaseArray = lowercaseCharsString.split('');
   var numbersArray = numbersString.split('');
   var uppercaseArray = uppercaseCharsString.split('');
 
-      // #1 if the user just wants lowercase letters
-  if (includeLowercase && includeUppercase === false && includeNumeric === false & includeSpecial === false) {
-    for(var i = 0; i < passwordLength; i++) {
-      //use a random number to select from arrays/strings up to the length
-      generatedPass.push((lowercaseArray[Math.floor(Math.random() * lowercaseArray.length)]));
-    }
+  //checking for each option user chose and if true, adding into array we will use to make our password...
+
+  //lowercase
+  if (includeLowercase)  {
+    eligiblevalues.push(...lowercaseArray);
   }
 
-  //#2 if the user wants lowercase and numeric
-  if (includeLowercase && includeUppercase === false && includeNumeric === true & includeSpecial === false) {
-    for(var i = 0; i < passwordLength; i++) {
-        //add the arrays we need
-      newArray = numbersArray.concat(lowercaseArray);
-      //use a random number to select from arrays/strings up to the length
-      generatedPass.push(newArray[Math.floor(Math.random() * newArray.length)]);
-    }
+  //uppercase
+  if (includeUppercase) {
+    eligiblevalues.push(...uppercaseArray);
   }
 
-  //#3 if the user wants lowercase, uppercase, and numeric
-  if (includeLowercase && includeUppercase === true && includeNumeric === true & includeSpecial === false) {
-    for(var i = 0; i < passwordLength; i++) {
-        //add the arrays we need
-      newArray = numbersArray.concat(lowercaseArray, uppercaseArray);
-      //use a random number to select from arrays/strings up to the length
-      generatedPass.push(newArray[Math.floor(Math.random() * newArray.length)]);
-    }
+  //numbers
+  if (includeNumeric) {
+    eligiblevalues.push(...numbersArray);
   }
 
-  //#4 if user wants lowercase, uppercase, numeric, and specialchars
-  if (includeLowercase && includeUppercase === true && includeNumeric === true & includeSpecial === true) {
-    for(var i = 0; i < passwordLength; i++) {
-        //add the arrays we need
-      newArray = numbersArray.concat(lowercaseArray, uppercaseArray, specialCharArray);
-      //use a random number to select from arrays/strings up to the length
-      generatedPass.push(newArray[Math.floor(Math.random() * newArray.length)]);
-    }
+  //specialchars
+  if (includeSpecial) {
+    eligiblevalues.push(...specialCharArray);
   }
 
-  //#5 if we have lowercase and uppercase
-  if (includeLowercase && includeUppercase === true && includeNumeric === false & includeSpecial === false) {
-    for(var i = 0; i < passwordLength; i++) {
-        //add the arrays we need
-      newArray = lowercaseArray.concat(uppercaseArray);
-      //use a random number to select from arrays/strings up to the length
-      generatedPass.push(newArray[Math.floor(Math.random() * newArray.length)]);
-    }
+  // Just confirming that our new array has what it needs
+  // console.log(eligiblevalues);
+
+  //Now we make a password from the magic sauce array we made
+
+  for(var i = 0; i < passwordLength; i++) {
+    //use a random number to select from arrays/strings up to the length
+    generatedPass.push((eligiblevalues[Math.floor(Math.random() * eligiblevalues.length)]));
   }
 
-  //#6 if we have lowercase and special...
-  //this is going to be 16 functions....  
+  //return the pretty password
   return(generatedPass.join(''));
 }
 
