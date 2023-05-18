303fec34a89fed4b2c893c49c1d54819f8b3b9f9
LeAnne B <branchleannelmb@gmail.com>
2023-05-17 21:59:46 -0400

    making sure first working copy is committed

--- script4.js
+++
@@ -1,5 +0,0 @@
-var passwordLength = prompt("Please input a password length (at least 8 characters but no more than 128 characters).");
-var includeLowercase = confirm("Include lowercase letters?");
-var includeUppercase = confirm("Include uppercase letters?");
-var includeNumeric = confirm("Include numbers?");
-var includeSpecial = confirm("Include special characters?");
